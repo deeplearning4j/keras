6d6cd301cd4d6c5f60794a58a9c47cfb51fba166
Max Pumperla <max.pumperla@googlemail.com>
2017-08-21 22:05:28 +0200

    more tests

--- dl4j-modelimport/make_simple_lstm.py
+++ dl4j-modelimport/make_simple_lstm.py
@@ -1,6 +1,6 @@
 '''
-Create a simple imdb fasttext net for testing Keras model import. Run
-Keras imdb_lstm.py example and then save that model and its
+Create a simple lstm net for testing Keras model import. Run
+Keras simple_lstm.py example and then save that model and its
 outputs to disk.
 '''
 from __future__ import print_function
@@ -9,9 +9,9 @@ import imp
 import keras.backend as K
 from util import save_model_details, save_model_output
 
-SCRIPT_PATH = '../examples/imdb_fasttext.py'
+SCRIPT_PATH = '../examples/simple_lstm.py'
 KERAS_VERSION = '_keras_2'
-PREFIX = 'imdb_fasttext_' + K.image_dim_ordering() + KERAS_VERSION
+PREFIX = 'simple_lstm_' + K.image_dim_ordering() + KERAS_VERSION
 OUT_DIR = '.'
 
 print('Entering Keras script')
@@ -21,6 +21,6 @@ print('Saving model details')
 save_model_details(example.model, prefix=PREFIX, out_dir=OUT_DIR)
 
 print('Saving model outputs')
-save_model_output(example.model, example.x_test, example.y_test, nb_examples=100, prefix=PREFIX, out_dir=OUT_DIR)
+save_model_output(example.model, example.cos, example.expected_output, nb_examples=100, prefix=PREFIX, out_dir=OUT_DIR)
 
 print('DONE!')
